0b3ba83de66aacfd2493e41c08564f188627b429
0mark96 <verardimarco3@gmail.com>
2022-04-12 14:09:16 +0200

    q

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+//close menu icon
 const navMenu = document.getElementById('navMenu');
 
 navMenu.addEventListener('click', ()=>{
     navMenu.classList.toggle('active');
 })
+
+// hide header onScroll
+const header = document.querySelector('header')
+let prevScrollPos = window.pageYOffset
+window.onscroll = function(){
+ let currentScrolPos = window.pageYOffset
+ if(prevScrollPos > currentScrolPos){
+     header.style.top='0'
+ }else{
+     header.style.top = '-50px'
+ }
+ prevScrollPos = currentScrolPos
+} 
+
