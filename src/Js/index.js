d0cc0520ba6e14bfaabcd2ffa1c5fd3793f99532
0mark96 <verardimarco3@gmail.com>
2022-05-06 14:41:47 +0200

    w

--- src/Js/index.js
+++ src/Js/index.js
@@ -37,8 +37,6 @@ function preLoader() {
       content.style.display = "block";
       content.style.transition='all .1s ease'
       hello.style.fontSize='1.6em'
-      location.href='#skill'
-      window.reload()
     }
   }    
      
