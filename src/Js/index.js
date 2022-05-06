f2eea650b229067271dbd0e7c6785051fe928ae0
0mark96 <verardimarco3@gmail.com>
2022-05-06 12:41:36 +0200

    w

--- src/Js/index.js
+++ src/Js/index.js
@@ -1,14 +1,5 @@
 
-//LOADERS----
-    function preLoader() {
-        setTimeout(showPage,3800);
-        }
-        
-        function showPage() {
-          document.getElementById("loaderContainer").style.display = "none";
-          document.getElementById("content").style.display = "block";
-       
-          //-----HOME PAGE----animation----
+// -----HOME PAGE----animation----
            const hello = document.querySelector('.hello')
            const homeSentence = document.querySelectorAll('#homeSentence')
            const helloTop = hello.getBoundingClientRect().top
@@ -26,7 +17,35 @@
               })
             },2200)
         }
-    }
+
+// //LOADERS----
+//     function preLoader() {
+//         setTimeout(showPage,3800);
+//         }
+        
+//         function showPage() {
+//           document.getElementById("loaderContainer").style.display = "none";
+//           document.getElementById("content").style.display = "block";
+       
+//           //-----HOME PAGE----animation----
+//            const hello = document.querySelector('.hello')
+//            const homeSentence = document.querySelectorAll('#homeSentence')
+//            const helloTop = hello.getBoundingClientRect().top
+//            const windowHeigth = window.innerHeight 
+//            if(helloTop < windowHeigth){
+//               homeSentence.forEach((item)=>{
+//               item.style.fontSize='0px'
+//            })
+//             setTimeout(()=>{
+//               hello.style.transition='all 1s ease'
+//               hello.style.fontSize='1.6em'
+//               homeSentence.forEach((item)=>{
+//                 item.style.transition='all 0.5s ease'
+//                 item.style.fontSize=''
+//               })
+//             },2200)
+//         }
+//     }
 
 
 // ----------------------Header effect mobile-------------------------------------
